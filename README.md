9ad7f36d657b6051d0f645e87529e71dcaddb6a2
Reiner Rottmann <reiner@rottmann.it>
2017-01-06 06:27:49 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
 # managetags
-Python script to manage tags encoded in the filename
+Python script to manage tags encoded in the filename.
+
+A tag is kept in a list of keywords enclosed in '[' and ']'
+at the end of the filename.
+
+Tags may be used to organize the file and to easier locate it.
+
+See also: https://github.com/rrottmann/filewrangler
+
+# Usage
+
+~~~
+$ ./managetags.py --help
+usage: managetags.py [-h] [--debug] [--nopreserve] [--nosorttags]
+                     [--notagctime] --path PATH [--quiet] [--addtags ADDTAGS]
+                     [--removetags REMOVETAGS]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --debug               Enable debug output.
+  --nopreserve          Do not preserve original tags.
+  --nosorttags          Do not sort tags.
+  --notagctime          Do not add tag for ctime.
+  --path PATH           Path to file/dir to tag.
+  --quiet               Quiet mode.
+  --addtags ADDTAGS     Comma seperated list of tags to add.
+  --removetags REMOVETAGS
+                        Comma seperated list of tags to remove.
+~~~
+
+# Examples
+
+## Add file's creation time as tag
+
+ * Create a test file
+
+~~~
+touch test.txt
+~~~
+
+ * Run managetags only with the path argument and see that a ctime tag gets proposed
+
+~~~
+ ./managetags.py --path test.txt
+mv "test.txt" "test [170106].txt"
+~~~
+
+ * As you are pleased with the result, we now execute the generated commands.
+
+~~~
+$ ./managetags.py --path test.txt | sh -x
++ mv test.txt 'test [170106].txt'
+ls -la test*
+-rw-r--r--. 1 root root 0 Jan  6 06:14 test [170106].txt
+~~~
+
+## Add tags 'foo', 'bar' and 'baz'
+
+  * Add 'foo' and 'bar' first
+
+~~~
+$ ./managetags.py --path "test [170106].txt" --addtags 'foo,bar'
+mv "test [170106].txt" "test [170106 bar foo].txt"
+~~~
+
+ * As you are pleased with the result, we now execute the generated commands.
+
+~~~
+$ ./managetags.py --path "test [170106].txt" --addtags 'foo,bar' | sh -x
++ mv 'test [170106].txt' 'test [170106 bar foo].txt'
+$ ls -al test*
+-rw-r--r--. 1 root root 0 Jan  6 06:14 test [170106 bar foo].txt
+
+ * Now add the tag 'baz' but we also want to remove the ctime tag. We want also debug output
+
+~~~
+$ ./managetags.py --path "test [170106 bar foo].txt" --addtags 'baz' --removetags '170106' --notagctime --debug
+#DEBUG: File has been tagged before.
+#DEBUG: Using old tags: 170106,bar,foo
+#DEBUG: Adding tag: baz
+#DEBUG: Removing tag: 170106
+#DEBUG: Sorting tags.
+mv "test [170106 bar foo].txt" "test [bar baz foo].txt"
+~~~
+
+ * As we are happy with it, we execute the generated commands. Note the debug output gets ignored.
+
+~~~
+./managetags.py --path "test [170106 bar foo].txt" --addtags 'baz' --removetags '170106' --notagctime --debug | sh -x
+#DEBUG: File has been tagged before.
+#DEBUG: Using old tags: 170106,bar,foo
+#DEBUG: Adding tag: baz
+#DEBUG: Removing tag: 170106
+#DEBUG: Sorting tags.
++ mv 'test [170106 bar foo].txt' 'test [bar baz foo].txt'
+~~~
+
+ * Inspect the results
+
+~~~
+$ ls -al test*
+-rw-r--r--. 1 root root 0 Jan  6 06:14 test [bar baz foo].txt
+~~~
---
+++ managetags.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright 2017 by Reiner Rottmann <reiner@rottmann.it>.
+Released under GPLv3 or later: https://www.gnu.org/licenses/gpl-3.0.txt
+"""
+
+import os
+import re
+import sys
+import time
+import logging
+import argparse
+import datetime
+
+__version__ = "0.1"
+
+
+def _get_ctime(path):
+    '''Get the creation time of the given file.'''
+    fmt = "%y%m%d"
+    ctime = datetime.datetime.strptime(time.ctime(os.path.getctime(path)), "%c").strftime(fmt)
+    return ctime
+
+
+def _manage_tags(path, addtags=None, removetags=None, tagctime=True, sorttags=True, preservetags=True):
+    '''
+    Add/Remove given tags to the file described by path.
+    Decide whether to append a tag with the file's ctime.
+    Decide whether to sort the tags alphabetically.
+    Decide whether to preserve original tags.
+    '''
+    if removetags is None:
+        logging.debug('No tags to remove.')
+        removetags = []
+    if addtags is None:
+        addtags = []
+    if tagctime:
+        addtags = addtags + [_get_ctime(path)]
+    for tag in addtags:
+        if tag in removetags:
+            if tag in addtags:
+                addtags.remove(tag)
+    if len(addtags) == 0 and not removetags:
+        logging.debug('No tags to add or remove.')
+        return path
+    regex = '(?P<name>^.*)\[(?P<tags>[^\[]+)\](?P<ext>.*$)'
+    fname = os.path.basename(path)
+    dir = os.path.dirname(path)
+    m = re.search(regex, fname)
+    if m:
+        logging.debug('File has been tagged before.')
+        oldtags = m.group('tags').split()
+        if preservetags:
+            logging.debug('Using old tags: ' + ','.join(oldtags))
+        else:
+            logging.debug('Ignoring old tags: ' + ','.join(oldtags))
+            oldtags = []
+        newtags = oldtags
+        for tag in addtags:
+            if tag not in oldtags:
+                if tag not in removetags:
+                    logging.debug('Adding tag: ' + tag)
+                    newtags.append(tag)
+        for tag in removetags:
+            if tag in newtags:
+                logging.debug('Removing tag: ' + tag)
+                newtags.remove(tag)
+        tags = newtags
+        if sorttags:
+            logging.debug('Sorting tags.')
+            tags.sort()
+        newfname = m.group('name').strip() + ' [' + ' '.join(newtags)+']'+m.group('ext').strip()
+    else:
+        logging.debug('The file is tagged for the first time.')
+        ext = '.' + fname.split('.')[-1]
+        name = fname[0:-len(ext)]
+        if sorttags:
+            logging.debug('Sorting tags.')
+            addtags.sort()
+        newfname = name + ' [' + ' '.join(addtags).strip() + ']' + ext
+        logging.debug('Old filename: ' + fname)
+        logging.debug('New filename: ' + newfname)
+    cmd = 'mv "%s" "%s"' % (os.path.join(dir, fname), os.path.join(dir, newfname))
+    return cmd
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', help='Enable debug output.', action='store_true')
+    parser.add_argument('--nopreserve', help='Do not preserve original tags.', action='store_false')
+    parser.add_argument('--nosorttags', help='Do not sort tags.', action='store_false')
+    parser.add_argument('--notagctime', help='Do not add tag for ctime.', action='store_false')
+    parser.add_argument('--path',
+                        help='Path to file/dir to tag.',
+                        default='.',
+                        required=True)
+    parser.add_argument('--quiet', help='Quiet mode.', action='store_true')
+    parser.add_argument('--addtags',
+                        help='Comma seperated list of tags to add.',
+                        default=None)
+    parser.add_argument('--removetags',
+                        help='Comma seperated list of tags to remove.',
+                        default=None)
+    args = parser.parse_args()
+    if args.quiet:
+        logging.basicConfig(level=logging.CRITICAL, format='#%(levelname)s: %(message)s')
+    else:
+        if args.debug:
+            logging.basicConfig(level=logging.DEBUG, format='#%(levelname)s: %(message)s')
+        else:
+            logging.basicConfig(level=logging.INFO, format='#%(levelname)s: %(message)s')
+    if not os.path.exists(args.path):
+        logging.error('No such file or directory: ' + args.path)
+        sys.exit(1)
+    if args.addtags is not None:
+        addtags = args.addtags.split(',')
+    else:
+        addtags = []
+    if args.removetags is not None:
+        removetags = args.removetags.split(',')
+    else:
+        removetags = []
+    print _manage_tags(args.path, addtags, removetags, args.notagctime, args.nosorttags, args.nopreserve)
